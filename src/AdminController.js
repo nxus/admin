14902bb61c799b05412df5f95b904419f788178c
Mike Reich <mjreich@gmail.com>
2016-09-20 07:33:36 -0700

    Checkpoint: working admin.page and admin.manage

--- src/AdminController.js
+++ src/AdminController.js
@@ -1,16 +1,19 @@
 import {EditController} from 'nxus-web'
-import {nav} from 'nxus-web/lib/modules/web-nav'
+import {nav} from 'nxus-web'
 import morph from 'morph'
 
 class AdminController extends EditController {
   constructor(opts) {
-    let _modelIdentity = opts.modelIdentity || morph.toSnake(new.target.name)
+    let _modelIdentity = opts.modelIdentity || morph.toDashed(new.target.name)
 
     opts.routePrefix = opts.routePrefix || '/admin/'+_modelIdentity
     opts.pageTemplate = opts.pageTemplate || 'admin-page'
+
     super(opts)
+    
     this.displayName = morph.toTitle(this.modelIdentity)
     this.icon = opts.icon || 'fa fa-files-o'
+    
     nav.add('admin-sidebar', this.displayName, this.routePrefix, {subMenu: this.prefix+'-submenu', icon: this.icon})
     nav.add(this.prefix+'-submenu', 'Create', this.routePrefix+'/create', {icon: 'fa fa-plus'})
   } 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 'use strict';
 
+import Promise from 'bluebird'
 import {NxusModule} from 'nxus-core'
 import {router} from 'nxus-router'
+import {templater} from 'nxus-templater'
+import {nav} from 'nxus-web'
 
 import AdminController from './AdminController'
+
+import _ from 'underscore'
+import morph from 'morph'
 
 /**
  * The Base Admin class provides a web interface for managing Nxus applications and data.
@@ -12,23 +18,94 @@ class Admin extends NxusModule {
 
   constructor() {
     super()
+
+    if(this.config.adminUrl[0] != '/') this.config.adminUrl = "/"+this.config.adminUrl
+  }
+
+  _defaultConfig() {
+    return {
+      pageTemplate: 'admin-page', 
+      adminUrl: '/admin'
+    }
   }
 
   /**
-   * Register a raw route for inclusion in the admin site
-   * @param {String} route  The url path for the page
-   * @param {function} handler for rendering this page
+   * Registers a page with the admin interface.  Pages can be defined two different ways.
+   * 
+   * ## Template Partials
+   *
+   * `admin.page('/route', 'partial-name')`
+   *
+   * ## Route Handlers
+   *
+   * `admin.page('/route', (req, res) => {...})`
+   *
+   * ## Options
+   *
+   * The first argument can either be a string or an options hash.  If an options hash, it 
+   * must include a `route` attribute.
+   *
+   * Other options include:
+   *
+   * * `route`: Required. The route to the page.
+   * * `method`: the HTTP method to use for the route.
+   * * `label`: the nav label to use. Defaults to a title version of the route.
+   * * `nav`: set to `false` to skip creating a nav item: useful for routes with no display.
+   * * `order`: the order of the nav menu.
+   * * `icon`: the icon class to use for the page in the nav and other places.
+   * 
+   * @param {String|Object}  opts  The route or options for the page
+   * @param {String|Function}  responder  The string partial name or handler function for the route
    */
-  page(route, opts = {}, handler) {
-    if(!handler) {
-      handler = opts
-      opts = {}
-    }
-    let method = opts.method || 'get'
-    if(route[0] != "/") route = "/"+route
-    route = this.opts.basePath+route
-    this.log('registering admin route', route)
-    router.route(method, route, handler)
+  page(opts, responder) {
+    if(_.isString(opts)) opts = {route: opts}
+
+    if(!opts.label) opts.label = morph.toTitle(opts.route.replace(new RegExp('/', 'ig'), " "))
+
+    opts.route = this._cleanRoute(opts.route)
+
+    if(!opts.nav || opts.nav !== false) this._addNav(opts.label, opts.route, opts)
+
+    if(_.isString(responder)) return this._addPartial(responder, opts)
+    else return this._addHandler(responder, opts)
+  }
+
+  _addNav(label, route, opts) {
+    nav.add('admin-sidebar', label, route, opts)
+  }
+
+  _addPartial(partial, opts = {}) {
+    let route = opts.route
+    if(!route) throw new Exception('Admin page must specify a route')
+    router.route(route, (req, res) => {
+      return templater.render(partial, {req, template: this.config.pageTemplate, ...opts}).then(::res.send)
+    })
+  }
+
+  _addHandler(handler, opts = {method: 'get'}) {
+    let route = opts.route
+    if(!route) throw new Exception('Admin page must specify a route')
+    
+    if(handler.length == 3) return router.route(method, route, handler)
+    
+    router.route(method, route, (req, res) => {
+      return Promise.resolve().then(() => {
+        return handler({req, ...opts})
+      }).then((r) => {
+        if(_.isString(r))
+          return templater.render(this.config.pageTemplate, {content: r})
+        else
+          return r
+      }).then(::res.send)
+    })
+  }
+
+  _cleanRoute(route) {
+    if(route[0] != '/') route = "/"+route
+
+    if(route.indexOf(this.config.adminUrl) != 0) route = this.config.adminUrl+route
+
+    return route
   }
 
   /**
@@ -36,7 +113,7 @@ class Admin extends NxusModule {
    * @return {[type]}
    */
   manage (model) {
-
+    new AdminController({modelIdentity: model})
   }
 }
 
